f348a2515390347a646f14804f239b42e62d0987
Gustao-DEL-SANTOS <63751042+Gustao-DEL-SANTOS@users.noreply.github.com>
2020-10-02 17:54:12 -0300

    infoprof
    adicionado info prof

--- src/navigation/InfoProfessor.java
+++ src/navigation/InfoProfessor.java
@@ -1,4 +1,35 @@
 package navigation;
 
-public class InfoProfessor {
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class InfoProfessor extends JFrame {
+	
+	JPanel painelIp= new JPanel();
+	
+	Color cl = new Color(255,0,0);
+	
+	public InfoProfessor() {
+		
+		setSize(900, 600);
+		getContentPane().setBackground(cl);
+		
+		setVisible(true);
+	}
+	
+	public void painel() {
+		
+		//painelIp.setLayout(new );
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		new InfoProfessor();
+		
+	}
+	
 }
