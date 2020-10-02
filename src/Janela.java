619e81c9ac458c74d8924d097b697600edb7436d
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-10-02 17:10:09 -0300

    Janela Principal

--- src/Janela.java
+++ src/Janela.java
@@ -1,4 +1,14 @@
 import javax.swing.*;
 
 public class Janela extends JFrame {
+
+    int SCREEN_WIDTH = 900;
+    int SCREEN_HEIGHT = 600;
+
+    public Janela(){
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        setVisible(true);
+    }
 }
