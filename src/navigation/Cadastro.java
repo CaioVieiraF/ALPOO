e8e2fa3bab85086e0197ed428f37adcca621b89c
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-10-04 16:47:19 -0300

    tela de cadastro iniciado

--- src/navigation/Cadastro.java
+++ src/navigation/Cadastro.java
@@ -5,7 +5,29 @@ import java.awt.*;
 
 public class Cadastro extends JPanel{
 
-    public Cadastro(){
-        setBackground(Color.BLACK);
+    JTextField nome = new JTextField();
+    JTextField email = new JTextField();
+    JTextField senha = new JPasswordField();
+    JTextField confirmaSenha = new JPasswordField();
+    JButton cadastrar = new JButton("Cadastrar");
+    Color fundo = new Color(210, 230, 255);
+
+    int mainWidth = 100;
+    int mainHeight = 150;
+    int mainPosX, mainPosY;
+
+    public Cadastro(int mainPosX, int mainPosY){
+
+        this.mainPosX = (mainPosX/2) - mainWidth;
+        this.mainPosY = (mainPosY/2) - mainHeight;
+
+        nome.setBounds(mainPosX, mainPosY, mainWidth, mainHeight);
+
+        setBackground(fundo);
+        add(nome);
+        add(email);
+        add(senha);
+        add(confirmaSenha);
+        add(cadastrar);
     }
 }
