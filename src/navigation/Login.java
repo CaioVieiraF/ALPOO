6ae3a95a90354ed0a99b1bd12668d4fa90982a79
lucksaug <65199071+lucksaug@users.noreply.github.com>
2020-10-05 18:59:36 -0300

    Update Login.java
    posição dos componentes;
    cor de fundo e botao, fonte.

--- src/navigation/Login.java
+++ src/navigation/Login.java
@@ -1,8 +1,11 @@
 package navigation;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,25 +14,44 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class Login extends JPanel implements ActionListener{
+	Color fundo = new Color(210, 230, 255);
+	Color corBot = new Color(180, 210, 190);
 	JButton bLogar = new JButton("Entrar");
 	JButton bVoltar = new JButton("Voltar");
 	JButton beSenha = new JButton("Esqueceu a senha?");
-	JLabel lEmail = new JLabel("Digite o email");
+	JLabel lEmail = new JLabel("Digite o email: ");
 	JTextField email = new JTextField();
-	JLabel lSenha = new JLabel("Digite a senha");
+	JLabel lSenha = new JLabel("Digite a senha: ");
 	JPasswordField senha = new JPasswordField("Senha");
-		
+	Font fonte = new Font("Ubuntu Mono", Font.BOLD, 14);
+	
 	public Login(){
 		setLayout(null);
+		setBackground(fundo);
 		
+		bVoltar.setBounds(200,300,80,40);
+		beSenha.setBounds(350,300,190,40);
+		bLogar.setBounds(580,300,80,40);
+		lEmail.setBounds(200,100,150,40);
+		email.setBounds(350,100,300,40);
+		lSenha.setBounds(200,200,150,40);
+		senha.setBounds(350,200,300,40);
 		
-		bLogar.setBounds(10,10,10,10);
-		bVoltar.setBounds(10,10,10,10);
-		beSenha.setBounds(10,10,10,10);
-		lEmail.setBounds(10,50,100,20);
-		email.setBounds(10,70,100,30);
-		lSenha.setBounds(10,100,100,20);
-		senha.setBounds(10,120,100,30);
+		bVoltar.setFont(fonte);
+		bVoltar.setBorder(BorderFactory.createEmptyBorder());
+		bVoltar.setBackground(corBot);
+		beSenha.setFont(fonte);
+		beSenha.setBorder(BorderFactory.createEmptyBorder());
+		beSenha.setBackground(corBot);
+		bLogar.setFont(fonte);
+		bLogar.setBorder(BorderFactory.createEmptyBorder());
+		bLogar.setBackground(corBot);
+		lEmail.setFont(fonte);
+		email.setFont(fonte);
+		email.setBorder(BorderFactory.createEmptyBorder());
+		lSenha.setFont(fonte);
+		senha.setFont(fonte);
+		senha.setBorder(BorderFactory.createEmptyBorder());
 		
 		add(lEmail);
 		add(email);
