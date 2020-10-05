0980bc80ad1c56c36705f3fffa6ac018fd90bcdb
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-10-05 00:30:43 -0300

    tela de cadastro basica

--- src/navigation/Cadastro.java
+++ src/navigation/Cadastro.java
@@ -1,33 +1,55 @@
 package navigation;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
 import java.awt.*;
 
 public class Cadastro extends JPanel{
 
-    JTextField nome = new JTextField();
-    JTextField email = new JTextField();
-    JTextField senha = new JPasswordField();
-    JTextField confirmaSenha = new JPasswordField();
+    JTextField nome, email, senha, confirmaSenha;
     JButton cadastrar = new JButton("Cadastrar");
     Color fundo = new Color(210, 230, 255);
+    Color buttonColor = new Color(180, 210, 190);
+    Font font = new Font("Ubuntu Mono", Font.BOLD, 14);
 
-    int mainWidth = 100;
-    int mainHeight = 150;
-    int mainPosX, mainPosY;
+    int mainWidth = 300;
+    int mainItemHeight = 50;
+    int mainPosX, mainPosY, mainHeight;
 
     public Cadastro(int mainPosX, int mainPosY){
+        this.nome = new JTextField("nome");
+        this.email = new JTextField("email");
+        this.senha = new JPasswordField("senha");
+        this.confirmaSenha = new JPasswordField("confirmar senha");
 
-        this.mainPosX = (mainPosX/2) - mainWidth;
-        this.mainPosY = (mainPosY/2) - mainHeight;
+        this.mainPosX = mainPosX/2 - mainWidth/2;
+        this.mainPosY = mainPosY/2;
+        this.mainHeight = this.mainPosY - (mainItemHeight * 5)/2;
 
-        nome.setBounds(mainPosX, mainPosY, mainWidth, mainHeight);
+        JComponent[] components = {nome, email, senha, confirmaSenha, cadastrar};
+        addComponents(components);
+
+        cadastrar.setBackground(buttonColor);
 
         setBackground(fundo);
-        add(nome);
-        add(email);
-        add(senha);
-        add(confirmaSenha);
-        add(cadastrar);
+        this.add(nome);
+        this.add(email);
+        this.add(senha);
+        this.add(confirmaSenha);
+        this.add(cadastrar);
+        setLayout(null);
+//        add(email);
+//        add(senha);
+//        add(confirmaSenha);
+//        add(cadastrar);
+    }
+
+    void addComponents(JComponent[] itens){
+        int padding = 5;
+        for (int i = 0; i < itens.length; i++){
+            itens[i].setBounds(this.mainPosX-padding, this.mainHeight+(this.mainItemHeight*i), mainWidth, mainItemHeight-padding*2);
+            itens[i].setBorder(BorderFactory.createEmptyBorder());
+            itens[i].setFont(font);
+        }
     }
 }
