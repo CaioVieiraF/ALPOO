4d3dcc675b1862838d7a0d8af067e649091088d4
Gustao-DEL-SANTOS <63751042+Gustao-DEL-SANTOS@users.noreply.github.com>
2020-10-06 15:22:46 -0300

    Info prof update
    add fields

--- src/navigation/InfoProfessor.java
+++ src/navigation/InfoProfessor.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -13,26 +14,14 @@ import javax.swing.JTextField;
 public class InfoProfessor extends JPanel implements ActionListener{
 	
 	// Labels Textos
-	JLabel lblIdProfessor = new JLabel("ID:");
-	JLabel lblNomeProfessor = new JLabel("NOME:");
-	JLabel lblEnderecoProfessor = new JLabel("ENDEREÇO:");
-	
-	JLabel lblRuaProfessor = new JLabel("Rua/Av:");
-	JLabel lblNumeroProfessor = new JLabel("NÚMERO:");
-	JLabel lblBairroProfessor = new JLabel("BAIRRO:");
-	JLabel lblCidadeProfessor = new JLabel("CIDADE:");
-	JLabel lblEstadoProfessor = new JLabel("ESTADO:");
-	
-	JLabel lblTelefoneProfessor = new JLabel("TEL:");
-	JLabel lblCelulatProfessor = new JLabel("CEL:");
 	
 	// TExtArea Caixas de Texto
 	JTextField txtIdProfessor = new JTextField();
 	JTextField txtNomeProfessor  = new JTextField();
-	JTextField txtEnderecoProfessor = new JTextField();
-	
 	JTextField txtRuaProfessor = new JTextField();
+	
 	JTextField txtNumeroProfessor = new JTextField();
+	
 	JTextField txtBairroProfessor = new JTextField();
 	JTextField txtCidadeProfessor = new JTextField();
 	JTextField txtEstadoProfessor = new JTextField();
@@ -44,8 +33,9 @@ public class InfoProfessor extends JPanel implements ActionListener{
 	Font fonte = new Font("Arial", Font.BOLD, 25);
 	Font fonte2 = new Font("Arial", Font.CENTER_BASELINE, 12);
 	
+	Color corF = new Color(230, 230, 230);
+	
 	public InfoProfessor() {
-		
 		painel();
 		Componentes();
 		setVisible(true);	
@@ -59,32 +49,77 @@ public class InfoProfessor extends JPanel implements ActionListener{
 	// Todos os componentes da tela
 	public void Componentes() {
 		
+		setBackground(new Color(200, 200, 200));
+		
 		// ID
-		lblIdProfessor.setBounds(10, 20, 40, 30);
-		lblIdProfessor.setFont(fonte);
-		txtIdProfessor.setBounds(50, 20, 100, 30);
+		txtIdProfessor.setBounds(10, 20, 70, 40);
 		txtIdProfessor.setFont(fonte2);
+		txtIdProfessor.setBorder(BorderFactory.createEmptyBorder());
+		txtIdProfessor.setBorder(BorderFactory.createTitledBorder("ID"));
+		txtIdProfessor.setBackground(corF);
 		
 		// NOME
-		lblNomeProfessor.setBounds(160, 20, 100, 30);
-		lblNomeProfessor.setFont(fonte);
-		txtNomeProfessor.setBounds(250, 20, 150, 30);
+		txtNomeProfessor.setBounds(80, 20, 210, 40);
 		txtNomeProfessor.setFont(fonte2);
+		txtNomeProfessor.setBorder(BorderFactory.createEmptyBorder());
+		txtNomeProfessor.setBorder(BorderFactory.createTitledBorder("NOME"));
+		txtNomeProfessor.setBackground(corF);
 		
-		// ENDERECO
-		lblEnderecoProfessor.setBounds(410, 20, 150, 30);
-		lblEnderecoProfessor.setFont(fonte);
-		txtEnderecoProfessor.setBounds(570, 20, 300, 30);
-		txtEnderecoProfessor.setFont(fonte2);
+		// RUA/AV
+		txtRuaProfessor.setBounds(290, 20, 280, 40);
+		txtRuaProfessor.setFont(fonte2);
+		txtRuaProfessor.setBorder(BorderFactory.createEmptyBorder());
+		txtRuaProfessor.setBorder(BorderFactory.createTitledBorder("RUA / AV"));
+		txtRuaProfessor.setBackground(corF);
+		
+		// BAIRRO
+		txtBairroProfessor.setBounds(570, 20, 300, 40);
+		txtBairroProfessor.setFont(fonte2);
+		txtBairroProfessor.setBorder(BorderFactory.createEmptyBorder());
+		txtBairroProfessor.setBorder(BorderFactory.createTitledBorder("BAIRRO"));
+		txtBairroProfessor.setBackground(corF);
+		
+		//CIDADE		
+		txtCidadeProfessor.setBounds(10, 60, 200, 40);
+		txtCidadeProfessor.setFont(fonte2);
+		txtCidadeProfessor.setBorder(BorderFactory.createEmptyBorder());
+		txtCidadeProfessor.setBorder(BorderFactory.createTitledBorder("cidade"));
+		txtCidadeProfessor.setBackground(corF);		
+		
+		//ESTADO		
+		txtEstadoProfessor.setBounds(210, 60, 200, 40);
+		txtEstadoProfessor.setFont(fonte2);
+		txtEstadoProfessor.setBorder(BorderFactory.createEmptyBorder());
+		txtEstadoProfessor.setBorder(BorderFactory.createTitledBorder("cidade"));
+		txtEstadoProfessor.setBackground(corF);		
+		
+		// NÚMERO
+		txtNumeroProfessor.setBounds(410, 60, 200, 40);
+		txtNumeroProfessor.setFont(fonte2);
+		txtNumeroProfessor.setBorder(BorderFactory.createEmptyBorder());
+		txtNumeroProfessor.setBorder(BorderFactory.createTitledBorder("TELELEFONE"));
+		txtNumeroProfessor.setBackground(corF);
+		
+		// CELULAR
+		txtCelulatProfessor.setBounds(610, 60, 200, 40);
+		txtCelulatProfessor.setFont(fonte2);
+		txtCelulatProfessor.setBorder(BorderFactory.createEmptyBorder());
+		txtCelulatProfessor.setBorder(BorderFactory.createTitledBorder("CELULAR"));
+		txtCelulatProfessor.setBackground(corF);
+		
+		
 		
 		
 		// OBJETOS TEXTO E CAIXA DE TEXTO
-		add(lblIdProfessor);
-		add(txtIdProfessor);
-		add(lblNomeProfessor);
+		add(txtIdProfessor);	
 		add(txtNomeProfessor);
-		add(lblEnderecoProfessor);
-		add(txtEnderecoProfessor);
+		add(txtBairroProfessor);		
+		add(txtRuaProfessor);
+		add(txtCidadeProfessor);
+		add(txtEstadoProfessor);
+		add(txtNumeroProfessor);
+		add(txtCelulatProfessor);
+		
 		
 	}
 
