26268d51427bdbded7b7684dc648a043b697ec91
Gustao-DEL-SANTOS <63751042+Gustao-DEL-SANTOS@users.noreply.github.com>
2020-10-23 16:35:13 -0300

    ADD listas profDisc

--- src/navigation/ProfessorDisciplina.java
+++ src/navigation/ProfessorDisciplina.java
@@ -17,36 +17,37 @@ public class ProfessorDisciplina extends JPanel {
 	
 	// Colunas da tabela
 	Object [][]  diciplinas = {
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"},
-            {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345", "joaosilva@hotmail.com", "48 8890-3345"}
+            {"João da Silva", "Estrututra de Dados", "LPBD", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta"},
+            {"Jorgin Ferrera", "Estrututra de Dados", "LPBD", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta"},       
+            {"Sr.Ramaus", "ALPOO"," taw ", "ipe", "lpoo", "libras (optativa)"},
+            {"Mr.Silva", "Estrututra de Dados", "LPBD", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta"},
+            {"Sr.Ramaus", "ALPOO"," taw ", "ipe", "lpoo", "libras (optativa)"},
+            {"Marie", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"Alexandre", "Cálculo p/ Computaçãoo", "Matemática Discreta", "ALPOO"," taw ", "ipe"},
+            {"André", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"Antônio", " taw ", "ipe", "lpoo", "libras (optativa)","Direitos Humanos (optativa)"},
+            {"Enzo", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta", "ALPOO"," taw "},
+            {"Henry",  "Estrututra de Dados", "LPBD", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta"},
+            {"Gabriel", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"Enzo Miguel ", "ALPOO"," taw ", "ipe", "lpoo", "libras (optativa)"},
+            {"Agatha", "Estrututra de Dados", "LPBD", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta"},
+            {"Antonela", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta", "ALPOO"," taw "},
+            {"Giovanna", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"Lavínia", "ALPOO"," taw ", "ipe", "lpoo", "libras (optativa)"},
+            {"Rebeca", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"Sophie", "ALPOO"," taw ", "ipe", "lpoo", "libras (optativa)"},
+            {"Vitória", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"Maria Flor", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta", "ALPOO"," taw "},
+            {"Maria Isis", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"	Maria Luiza", "ALPOO"," taw ", "ipe", "lpoo", "libras (optativa)"},
+            {"Maria Sophia", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"Maria Valentina", "ALPOO"," taw ", "ipe", "lpoo", "libras (optativa)"},
+            {"Ana Luiza", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"Cora", " taw ", "ipe", "lpoo", "libras (optativa)","Direitos Humanos (optativa)"},
+            {"Analu", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta", "ALPOO"," taw "},
+            {"Anastácia",  "Estrututra de Dados", "LPBD", "Ciências Sociais", "Cálculo p/ Computaçãoo", "Matemática Discreta"},
+            {"Amélia", "Matemática Discreta", "ALPOO"," taw ", "ipe", "lpoo"},
+            {"Julieta", "ALPOO"," taw ", "ipe", "lpoo", "libras (optativa)"}
             };
 	
 	// Titulo da tabel
@@ -70,10 +71,8 @@ public class ProfessorDisciplina extends JPanel {
 		pCentral.setBounds(100, 10, 680, 520);
 		pCentral.setBackground(decoracao.corF);
 		pCentral.setLayout(null);
-		pCentral.setBorder(BorderFactory.createTitledBorder("Professor/Disciplina"));
-        			
+		pCentral.setBorder(BorderFactory.createTitledBorder("Professor/Disciplina"));		
         CriarTabela(titulos, diciplinas);
-
         add(pCentral);
 	}
 	
