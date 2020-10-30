5fdda952e6fd42d6c188b5e67021ffde0e616178
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-10-30 15:16:18 -0300

    getDiciplinas consertado

--- src/backend/DiciplinasHandler.java
+++ src/backend/DiciplinasHandler.java
@@ -60,7 +60,7 @@ public class DiciplinasHandler {
         }
     }
 
-    public void getDiciplinas(Diciplina[] returnVal){
+    public Diciplina[] getDiciplinas(){
         Connection conn = null;
         Statement stmt = null;
         try {
@@ -93,7 +93,7 @@ public class DiciplinasHandler {
                 diciplinas[diciplinas.length - 1] = diciplina;
             }
 
-            returnVal = diciplinas;
+            return diciplinas;
         } catch (Exception se) {
             se.printStackTrace();
         }
@@ -114,5 +114,6 @@ public class DiciplinasHandler {
                 se.printStackTrace();
             }
         }
+        return null;
     }
 }
